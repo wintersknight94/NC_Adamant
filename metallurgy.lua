0d52ba7e625bddc4507e2d8d3ba01878a788d02c
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2021-12-28 11:12:38 -0600

    Fixed Container Deletion Bug
    Containers should no longer be deleted when a hot prill cools in them.

--- metallurgy.lua
+++ metallurgy.lua
@@ -1,6 +1,6 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, nodecore, pairs, type
-    = minetest, nodecore, pairs, type
+local ItemStack, error, minetest, nodecore, pairs, type
+    = ItemStack, error, minetest, nodecore, pairs, type
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
@@ -24,7 +24,7 @@ local tempers = {
 	}
 }
 
-function nodecore.register_adamant(shape, rawdef)
+function nodecore.register_lode(shape, rawdef)
 	rawdef.groups = rawdef.groups or {}
 	for _, temper in pairs(tempers) do
 		local def = nodecore.underride({}, rawdef)
@@ -81,7 +81,7 @@ function nodecore.register_adamant(shape, rawdef)
 	end
 end
 
-nodecore.register_adamant("Block", {
+nodecore.register_lode("Block", {
 		type = "node",
 		description = "## Adamant",
 		tiles = {modname .. "_#.png"},
@@ -90,11 +90,65 @@ nodecore.register_adamant("Block", {
 		crush_damage = 4
 	})
 
-nodecore.register_adamant("Prill", {
+nodecore.register_lode("Prill", {
 		type = "craft",
 		groups = {metal_prill = 1},
 		light_source = 1,
 		inventory_image = modname .. "_#.png^[mask:" .. modname .. "_mask_prill.png",
+	})
+
+local function replacestack(pos, temper)
+	local stack = nodecore.stack_get(pos)
+	if stack:is_empty() then stack = nil end
+	local node = minetest.get_node(pos)
+	local name = stack and stack:get_name() or node.name
+	local def = minetest.registered_items[name] or {}
+	local alt = def["metal_alt_" .. temper]
+	if not alt then return error("no " .. alt .. " alt for " .. name) end
+	if stack then
+		local repl = ItemStack(alt)
+		local qty = stack:get_count()
+		if qty == 0 then qty = 1 end
+		repl:set_count(qty * repl:get_count())
+		nodecore.stack_set(pos, repl)
+	else
+		nodecore.set_node(pos, {name = alt})
+		nodecore.fallcheck(pos)
+	end
+	nodecore.witness(pos, "metallurgize " .. alt)
+end
+
+nodecore.register_craft({
+		label = "adamant stack heating",
+		action = "cook",
+		touchgroups = {flame = 3},
+		duration = 30,
+		cookfx = true,
+		nodes = {{match = {metal_temper_cool = true, count = false}}},
+		after = function(pos) return replacestack(pos, "hot") end
+	})
+
+nodecore.register_craft({
+		label = "adamant stack annealing",
+		action = "cook",
+		touchgroups = {flame = 0},
+		duration = 120,
+		priority = -1,
+		cookfx = {smoke = true, hiss = true},
+		nodes = {{match = {metal_temper_hot = true, count = false}}},
+		after = function(pos) return replacestack(pos, "annealed") end
+	})
+
+nodecore.register_craft({
+		label = "adamant stack quenching",
+		action = "cook",
+		touchgroups = {flame = 0},
+		check = function(pos)
+			return nodecore.quenched(pos)
+		end,
+		cookfx = true,
+		nodes = {{match = {metal_temper_hot = true, count = false}}},
+		after = function(pos) return replacestack(pos, "tempered") end
 	})
 
 -- Because of how massive they are, forging a block is a hot-working process.
